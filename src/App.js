e572c6e90b3d8357341a76efdcede2ac699a2131
Ivan Montes <101290628+ivanivanmontes@users.noreply.github.com>
2025-02-10 19:41:12 -0500

    more formatting

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import Res from "./res";
 import Res2 from "./res2";
 import Res3 from "./res3";
 import LandingPage from "./LandingPage";
+import GitHubLink from "./link";
 
 
 
 function App() {
   return (
     <Router>
+
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/question" element={<Res />} />
         {/* <Route path="/no" element={<Res2 />} /> */}
         <Route path="/yes" element={<Res3 />} />
+        
       </Routes>
+      <GitHubLink />
     </Router>
   );
 }
---
+++ src/link.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const GitHubLink = () => {
+    return (
+      <a
+        href="https://github.com/ivanivanmontes/knuffle-bunny"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="github-link"
+      >
+        <img
+          src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" // GitHub logo
+          alt="GitHub Repo"
+          className="github-logo"
+        />
+      </a>
+    );
+  };
+  
+  export default GitHubLink;
